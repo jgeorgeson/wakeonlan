89ce75c8dab76b93b44375c5397ff1bd1a9d4771
justin <justin@b77f6b1f-9c79-0410-be51-b35558c37a78>
2009-12-01 01:13:49 +0000

    Main activity should now have the full set of stubbed overrides. Add empty layout and class for HostEdit activity, to be called by context menu Edit and options menu Add.

--- src/net/lopht/wakeonlan/wakeonlan.java
+++ src/net/lopht/wakeonlan/wakeonlan.java
@@ -1,14 +1,32 @@
 package net.lopht.wakeonlan;
 
 import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ContextMenu.ContextMenuInfo;
+import android.widget.ListView;
 
 public class wakeonlan extends ListActivity {
+	private static final int ACTIVITY_CREATE = 0;
+	private static final int ACTIVITY_EDIT = 1;
+	
+	private static final int MENU_WAKE = Menu.FIRST;
+	private static final int MENU_EDIT = Menu.FIRST + 1;
+	private static final int MENU_DELETE = Menu.FIRST + 2;
+	
+	/** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        updateList();
+        registerForContextMenu(getListView());
+    }
+    
     @Override
 	public boolean onContextItemSelected(MenuItem item) {
 		// TODO Auto-generated method stub
@@ -29,17 +47,25 @@ public class wakeonlan extends ListActivity {
 	}
 
 	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
+	public boolean onMenuItemSelected(int featureId, MenuItem item) {
 		// TODO Auto-generated method stub
-		return super.onOptionsItemSelected(item);
+		return super.onMenuItemSelected(featureId, item);
 	}
 
-	/** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		// TODO Auto-generated method stub
+		super.onListItemClick(l, v, position, id);
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		// TODO Auto-generated method stub
+		super.onActivityResult(requestCode, resultCode, data);
+		updateList();
+	}
+
+	private void updateList () {
+    	// TODO update the list view with the hosts from the database
     }
-    
-    
 }
