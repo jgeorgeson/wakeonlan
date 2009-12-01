f55cf608602d4de7a55613f1bd27057df13885ea
justin <justin@b77f6b1f-9c79-0410-be51-b35558c37a78>
2009-12-01 23:01:35 +0000

    Add the DB adapter class and HostEdit activity class. Fill in menu hooks. TODO:   prevent creation of blank hosts   Edit not pulling up existing host in editor

--- trunk/src/net/lopht/wakeonlan/HostEdit.java
+++ trunk/src/net/lopht/wakeonlan/HostEdit.java
@@ -1,15 +1,96 @@
 package net.lopht.wakeonlan;
 
 import android.app.Activity;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class HostEdit extends Activity {
-
+	private EditText mHostname, mIP, mMAC;
+	private Long mRowID;
+	private HostDbAdapter mDbAdapter;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.host_edit);
+		
+		/* Get the database */
+		mDbAdapter = new HostDbAdapter(this);
+		mDbAdapter.open();
+		
+		/* Get the form fields */
+		mHostname = (EditText) findViewById(R.id.hostname);
+		mIP = (EditText) findViewById(R.id.ip);
+		mMAC = (EditText) findViewById(R.id.mac);
+		
+		/* Saved state? */
+		mRowID = savedInstanceState != null ?
+				savedInstanceState.getLong(HostDbAdapter.KEY_ROWID):
+				null;
+
+		/* No saved state */
+        if (mRowID == null) {
+			Bundle extras = getIntent().getExtras();            
+			mRowID = extras != null ? extras.getLong(HostDbAdapter.KEY_ROWID) : null;
+		}
+        
+        /* Populate the fields with existing data, if any */
+        updateFields();
+		        
+		/* Get the button and setup a listener for it */
+		Button confirm = (Button) findViewById(R.id.confirm);
+		confirm.setOnClickListener(new View.OnClickListener() {
+			
+			public void onClick(View v) {
+                setResult(RESULT_OK);
+                finish();				
+			}
+		});
+	}
+	
+	private void updateFields () {
+		if (mRowID != null) {
+			Cursor host = mDbAdapter.fetchHost(mRowID);
+			startManagingCursor(host);
+			mHostname.setText(host.getString(host.getColumnIndexOrThrow(HostDbAdapter.KEY_HOSTNAME)));
+			mIP.setText(host.getString(host.getColumnIndexOrThrow(HostDbAdapter.KEY_IP)));
+			mMAC.setText(host.getString(host.getColumnIndexOrThrow(HostDbAdapter.KEY_MAC)));
+		}
 	}
 
+	@Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putLong(HostDbAdapter.KEY_ROWID, mRowID);
+    }
+    
+    @Override
+    protected void onPause() {
+        super.onPause();
+        saveState();
+    }
+    
+    @Override
+    protected void onResume() {
+        super.onResume();
+        updateFields();
+    }
+    
+    private void saveState() {
+        String hostname = mHostname.getText().toString();
+        String ip = mIP.getText().toString();
+        String mac = mMAC.getText().toString();
+
+        if (mRowID == null) {
+            long id = mDbAdapter.createHost(hostname, ip, mac);
+            if (id > 0) {
+                mRowID = id;
+            }
+        } else {
+            mDbAdapter.updateHost(mRowID, hostname, ip, mac);
+        }
+    }
 }
---
+++ trunk/src/net/lopht/wakeonlan/HostList.java
@@ -0,0 +1,108 @@
+package net.lopht.wakeonlan;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.AdapterView.AdapterContextMenuInfo;
+
+public class HostList extends ListActivity {
+	private static final int ACTIVITY_CREATE = 0;
+	private static final int ACTIVITY_EDIT = 1;
+	
+	private static final int MENU_WAKE = Menu.FIRST;
+	private static final int MENU_EDIT = Menu.FIRST + 1;
+	private static final int MENU_DELETE = Menu.FIRST + 2;
+	
+	private HostDbAdapter mDbAdapter;
+	
+	/** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        mDbAdapter = new HostDbAdapter(this);
+        mDbAdapter.open();
+        updateList();
+        registerForContextMenu(getListView());
+    }
+    
+	@Override
+	public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
+		super.onCreateContextMenu(menu, v, menuInfo);
+		menu.add(0,MENU_WAKE,0,R.string.menu_wake);
+		menu.add(0,MENU_EDIT,0,R.string.menu_edit);
+		menu.add(0,MENU_DELETE,0,R.string.menu_delete);
+	}
+
+    @Override
+	public boolean onContextItemSelected(MenuItem item) {
+		AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
+		switch(item.getItemId()) {
+		case MENU_WAKE:
+			return true;
+		case MENU_EDIT:
+	        Intent i = new Intent(this, HostEdit.class);
+	        i.putExtra(HostDbAdapter.KEY_ROWID, info.id);
+	        startActivityForResult(i, ACTIVITY_EDIT);
+	        return true;
+		case MENU_DELETE:
+			mDbAdapter.deleteHost(info.id);
+			updateList();
+			return true;
+		}
+		return super.onContextItemSelected(item);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		super.onCreateOptionsMenu(menu);
+		menu.add(0,MENU_EDIT,0,R.string.menu_add);
+		return true;
+	}
+
+	@Override
+	public boolean onMenuItemSelected(int featureId, MenuItem item) {
+		switch(item.getItemId()) {
+		case MENU_EDIT:
+			Intent i = new Intent(this,HostEdit.class);
+			startActivityForResult(i,ACTIVITY_CREATE);
+			return true;
+		}
+		return super.onMenuItemSelected(featureId, item);
+	}
+
+    @Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		super.onListItemClick(l, v, position, id);
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		updateList();
+	}
+
+	private void updateList () {
+        // Get all of the rows from the database and create the item list
+        Cursor hostsCursor = mDbAdapter.fetchAllHosts();
+        startManagingCursor(hostsCursor);
+        
+        // Create an array to specify the fields we want to display in the list (only hostname)
+        String[] from = new String[]{HostDbAdapter.KEY_HOSTNAME};
+        
+        // and an array of the fields we want to bind those fields to
+        int[] to = new int[]{R.id.host_row};
+        
+        // Now create a simple cursor adapter and set it to display
+        SimpleCursorAdapter hosts = 
+        	    new SimpleCursorAdapter(this, R.layout.host_row, hostsCursor, from, to);
+        setListAdapter(hosts);    }
+}
