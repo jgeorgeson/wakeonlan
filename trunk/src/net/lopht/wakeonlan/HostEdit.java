d620ffb8b09dc5e3d9e77aa57b293c5136607d7c
justin <justin@b77f6b1f-9c79-0410-be51-b35558c37a78>
2009-12-01 01:13:49 +0000

    Main activity should now have the full set of stubbed overrides. Add empty layout and class for HostEdit activity, to be called by context menu Edit and options menu Add.

---
+++ trunk/src/net/lopht/wakeonlan/HostEdit.java
@@ -0,0 +1,15 @@
+package net.lopht.wakeonlan;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HostEdit extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.host_edit);
+	}
+
+}
